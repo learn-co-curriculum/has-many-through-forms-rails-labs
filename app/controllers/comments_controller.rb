2328b4f9f304cd0b82aa43bae2da4531c848bd54
ipc103 <iancandysemail@gmail.com>
2016-01-19 13:42:12 -0500

    add fields_for user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id)
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 end
