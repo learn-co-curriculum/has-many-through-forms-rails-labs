6e37c88e045f762a3edde501aa7166d93313b133
ipc103 <iancandysemail@gmail.com>
2016-01-19 16:35:26 -0500

    remove solution

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,4 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  accepts_nested_attributes_for :user, reject_if: :all_blank
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,5 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
-    end
-  end
 
 end
