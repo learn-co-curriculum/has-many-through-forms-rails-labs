c84eef207c5166721977893c8f37ef5cc0ab0f9a
djsteve33 <swomble@aol.com>
2021-05-06 15:31:26 -0400

    commit tree 7763c2e63feee906a110796556e8e43502520166

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
 end
